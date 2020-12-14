4d54c81799ec9a7429a40f368384d4a7ea416436
Russell Shomberg <russellshomberg@gmail.com>
2020-12-14 00:53:46 -0500

    removed some iles

--- gen_halfspace_bdry.m
+++
@@ -1,8 +0,0 @@
-function bdry = gen_halfspace_bdry(cp,cs,rho,alpha,beta,depths)
-% gen_halfspace_bdry creates a acoustic halfspace AcousticBoundary
-% 
-% TODO: document
-
-    bdry = AcousticBoundary('halfspace',depth,cp,cs,rho,alpha,beta);
-    
-end
